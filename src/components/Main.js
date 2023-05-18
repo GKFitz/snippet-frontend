6e3c421077637ad640e49419a5a9426efc555f47
GKFitz123 <gkf2103@gmail.com>
2023-05-18 17:49:37 -0400

    seting up the components and pages, and index.js route/router

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
+export const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
